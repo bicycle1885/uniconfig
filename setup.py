16a7f4f162b3ef65266dd968c24fa3e944225fd3
佐藤 建太 <bicycle1885@gmail.com>
2013-07-06 17:26:26 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
+import uniconfig
 
 
 setup(
     name="uniconfig",
+    version=uniconfig.__version__,
+    author=uniconfig.__author__,
+    maintainer=uniconfig.__maintainer__,
+    description="Make it easy to make configuration objects from any sources.",
+    packages=find_packages(),
     install_requires=["PyYAML>=3.0"]
 )
